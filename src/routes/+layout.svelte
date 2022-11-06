b0e47b7bea354753e6155c10e8a17609c2f2189f
Chentse Lee <chentselee@gmail.com>
2022-11-06 10:23:26 +0800

    add bottom nav tab

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,5 +1,20 @@
 <script>
 	import '../app.css'
+	import {
+		faHouse,
+		faMagnifyingGlass,
+		faCartShopping,
+		faUser,
+	} from '@fortawesome/free-solid-svg-icons'
+	import TabItem from './TabItem.svelte'
 </script>
 
-<slot />
+<div class="min-h-screen">
+	<slot />
+	<nav class="py-2 fixed bottom-0 flex w-full text-center">
+		<TabItem href="/" icon={faHouse} text="首頁" />
+		<TabItem href="/explore" icon={faMagnifyingGlass} text="瀏覽" />
+		<TabItem href="/cart" icon={faCartShopping} text="購物車" />
+		<TabItem href="/account" icon={faUser} text="帳戶" />
+	</nav>
+</div>
---
+++ src/routes/TabItem.svelte
@@ -0,0 +1,18 @@
+<script lang="ts">
+	import Fa from 'svelte-fa'
+	import type { IconDefinition } from '@fortawesome/free-solid-svg-icons'
+	import { page } from '$app/stores'
+	export let icon: IconDefinition
+	export let text: string
+	export let href: HTMLAnchorElement['href']
+</script>
+
+<a
+	{href}
+	class={`grow flex flex-col items-center gap-1.5 ${
+		$page.url.pathname === href ? '' : 'text-gray-500'
+	}`}
+>
+	<Fa {icon} class="text-2xl" />
+	<span class="text-xs">{text}</span>
+</a>
